fcb2e9e4987d7b5262a5171c2b45cc430f9243ad
Faria Siddiqi <fariasiddiqi14@gmail.com>
2023-04-01 20:27:02 +0500

    Updated new files

--- pages/city.js
+++ pages/city.js
@@ -1,34 +1,27 @@
 // import React from 'react'
 
-import Eventmaterial from '../components/event-material'
-import Carousel from '../components/carousel'
+import Eventmaterial from "../components/event-material";
+import Carousel from "../components/carousel";
 // import Navbar from '../components/navbar'
-import Framecontainer from '@/components/frame-container'
-import Footerbanner from '../components/footer-banner'
-
-
-
+import Framecontainer from "@/components/frame-container";
+import Footerbanner from "../components/footer-banner";
 
 export default function HomePage() {
-    return (
-
-        <>
-            <div>
-                {/* <Navbar /> */}
-            </div>
-            <div>
-                <Carousel />
-            </div>
-            <div > 
-                <Eventmaterial />
-            </div>
-            <div>
-                <Framecontainer />
-            </div>
-            <div>
-                <Footerbanner />
-            </div>
-
-        </>
-    )
+  return (
+    <>
+      <div>{/* <Navbar /> */}</div>
+      <div>
+        <Carousel />
+      </div>
+      <div>
+        <Eventmaterial />
+      </div>
+      <div>
+        <Framecontainer />
+      </div>
+      <div>
+        <Footerbanner />
+      </div>
+    </>
+  );
 }
